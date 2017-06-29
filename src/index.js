d8d0c13d3b3ac0539ee670269bb57e6abcc507ad
Wassburgare <daniel.wassbjer@gmail.com>
2017-06-29 15:35:34 +0200

    Detect user locale
    Should perhaps check the Accept-Language header somehow

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import sv from 'react-intl/locale-data/sv';
 import en from 'react-intl/locale-data/en';
-import svMessages from './translations/locales/sv.json';
+import localeMessages from './translations/locales/localeMessages';
 
 import './index.css';
 import App from './App';
@@ -12,8 +12,17 @@ import registerServiceWorker from './registerServiceWorker';
 
 addLocaleData([...sv, ...en]);
 
+const language = (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage ||
+  navigator.browserLanguage ||
+  navigator.systemLanguage;
+
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+const messages = localeMessages[language] || localeMessages[languageWithoutRegionCode] || localeMessages.en;
+
 ReactDOM.render(
-  <IntlProvider locale={'sv'} messages={svMessages}>
+  <IntlProvider locale={language} messages={messages}>
     <App />
   </IntlProvider>,
   document.getElementById('root'),
