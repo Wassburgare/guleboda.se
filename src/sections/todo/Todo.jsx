d39315c79b61e5ccf163746bf5dc66698e40eb94
Wassburgare <daniel.wassbjer@gmail.com>
2017-06-29 13:33:07 +0200

    "Translate" links and headings for todo section

--- src/sections/todo/Todo.jsx
+++ src/sections/todo/Todo.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import Lorem from 'react-lorem-component';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl, defineMessages } from 'react-intl';
 import './Todo.css';
+
+const messages = defineMessages({
+  korroHref: {
+    id: 'Todo.korro.href',
+    defaultMessage: 'http://www.korro.se',
+  },
+  glasriketHref: {
+    id: 'Todo.glasriket.href',
+    defaultMessage: 'https://www.glasriket.se/en/The-Glassworks',
+  },
+  kostaHref: {
+    id: 'Todo.kosta.href',
+    defaultMessage: 'http://www.kostaoutlet.se',
+  },
+  utvandrarledenHref: {
+    id: 'Todo.utvandrarleden.href',
+    defaultMessage: 'http://www.utvandrarleden.se/en-GB',
+  },
+});
 
 class Todo extends Component {
   render() {
@@ -21,27 +40,69 @@ class Todo extends Component {
             <img src={'https://www.placecage.com/gif/450/280'} alt="" />
             <h3>Korrö Hantverksby</h3>
             <Lorem count="1" />
-            <a href="http://www.korro.se/" target="_blank" rel="noopener noreferrer">www.korro.se</a>
+            <a
+              href={this.props.intl.formatMessage(messages.korroHref)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FormattedMessage
+                id={'Todo.korro.linkText'}
+                defaultMessage={'www.korro.se'}
+              />
+            </a>
           </div>
           <div className="todo-item">
             <img src={'https://www.placecage.com/gif/451/280'} alt="" />
-            <h3>Glasriket Småland</h3>
+            <h3>
+              <FormattedMessage
+                id={'Todo.glasriket.heading'}
+                defaultMessage={'Kingdom of Crystal'}
+              />
+            </h3>
             <Lorem count="1" />
-            <a href="https://www.glasriket.se/sv/glaset/glasbruken" target="_blank" rel="noopener noreferrer">www.glasriket.se/sv/glaset/glasbruken</a>
+            <a
+              href={this.props.intl.formatMessage(messages.glasriketHref)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FormattedMessage
+                id={'Todo.glasriket.linkText'}
+                defaultMessage={'www.glasriket.se/en/The-Glassworks'}
+              />
+            </a>
           </div>
         </div>
+
         <div className="todo-item-row">
           <div className="todo-item">
             <img src={'https://www.placecage.com/gif/452/280'} alt="" />
             <h3>Kosta Outlet</h3>
             <Lorem count="1" />
-            <a href="http://www.kostaoutlet.se/" target="_blank" rel="noopener noreferrer">www.kostaoutlet.se</a>
+            <a
+              href={this.props.intl.formatMessage(messages.kostaHref)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FormattedMessage
+                id={'Todo.kosta.linkText'}
+                defaultMessage={'www.kostaoutlet.se'}
+              />
+            </a>
           </div>
           <div className="todo-item">
             <img src={'https://www.placecage.com/gif/453/280'} alt="" />
             <h3>Utvandrarleden</h3>
             <Lorem count="1" />
-            <a href="http://www.utvandrarleden.se/" target="_blank" rel="noopener noreferrer">www.utvandrarleden.se</a>
+            <a
+              href={this.props.intl.formatMessage(messages.utvandrarledenHref)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FormattedMessage
+                id={'Todo.utvandrarleden.linkText'}
+                defaultMessage={'www.utvandrarleden.se/en-GB'}
+              />
+            </a>
           </div>
         </div>
       </div>
@@ -49,4 +110,4 @@ class Todo extends Component {
   }
 }
 
-export default Todo;
+export default injectIntl(Todo);
