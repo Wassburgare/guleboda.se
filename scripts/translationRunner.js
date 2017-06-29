1ea0c0fc8c19359c31f9f625479fe3ca3b224049
Wassburgare <daniel.wassbjer@gmail.com>
2017-06-29 20:53:08 +0200

    Add trailing newline to locale files

--- scripts/translationRunner.js
+++ scripts/translationRunner.js
@@ -3,5 +3,6 @@ const manageTranslations = require('react-intl-translations-manager').default;
 manageTranslations({
   messagesDirectory: 'src/translations/src/',
   translationsDirectory: 'src/translations/locales/',
+  jsonOptions: { trailingNewline: true },
   languages: ['sv'],
 });
