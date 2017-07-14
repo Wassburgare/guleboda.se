49e3c03879ea3adfe131c1943342a75d59618692
Wassburgare <daniel.wassbjer@gmail.com>
2017-07-14 17:58:09 +0200

    Remove console.log

--- client/src/reducers/bookingsReducer.js
+++ client/src/reducers/bookingsReducer.js
@@ -3,7 +3,6 @@ export default (state = {}, action) => {
     case 'FETCH_BOOKINGS':
       return { ...state };
     case 'FETCH_BOOKINGS_SUCCESS':
-      console.log(action);
       return { ...state, list: action.payload };
     default:
       return state;
