600fd32c35cbc1aa844d6cbd9e497040aebd7b87
Wassburgare <daniel.wassbjer@gmail.com>
2017-07-14 18:01:53 +0200

    Make communication between client and server possible

--- client/src/db-connection/DbConnection.js
+++ client/src/db-connection/DbConnection.js
@@ -1,10 +1,8 @@
-import request from 'request';
-
 class DbConnection {
   static getBookings(callback = () => {}) {
-    request('http://localhost:8080/bookings', (error, response, body) => {
-      callback(JSON.parse(body));
-    });
+    fetch('/bookings')
+      .then(response => response.json())
+      .then(callback);
   }
 }
 
