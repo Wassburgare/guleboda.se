8abc82fe099bab72b9a34335bb7300a8a4fe05e4
Wassburgare <daniel.wassbjer@gmail.com>
2017-07-25 17:02:18 +0200

    Locale reducer needs to be there...

--- client/src/app/admin/reducers.js
+++ client/src/app/admin/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import locale from 'src/locale/reducer';
 import bookings from 'src/bookings/reducer';
 
 export default combineReducers({
+  locale,
   bookings,
 });
