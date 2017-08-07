43883ea68db3ff4172f7baa4cba6b207c2b49c33
Wassburgare <daniel.wassbjer@gmail.com>
2017-08-07 02:04:19 +0200

    Polish the remote update script a little

--- client/scripts/remoteUpdate.js
+++ client/scripts/remoteUpdate.js
@@ -6,54 +6,64 @@ const devnull = require('dev-null');
 
 const archivePath = path.resolve(__dirname, 'build.tar.gz');
 const serverHome = '/home/wassburgare';
-const commands = [
-  `tar -xvf ${serverHome}/build.tar.gz`,
-  'rm -rf /var/www/guleboda.se/html/*',
-  `mv ${serverHome}/build/guleboda.se/* /var/www/guleboda.se/html`,
-  'rm -rf /var/www/admin.guleboda.se/html/*',
-  `mv ${serverHome}/build/admin.guleboda.se/* /var/www/admin.guleboda.se/html`,
-  `rm -rf ${serverHome}/build`,
-  `rm ${serverHome}/build.tar.gz`,
-];
 const connection = new Client();
 
-tar.c(
-  {
-    gzip: true,
-    file: archivePath,
-  },
-  ['build'],
-).then(() => {
-  return new Promise((resolve, reject) => {
-    connection.on('ready', () => {
-      connection.sftp((err, sftp) => {
-        if (err) {
-          return reject(err);
-        }
+const uploadFile = (sftp, srcFile, destFile) =>
+  new Promise((resolve, reject) =>
+    sftp.fastPut(srcFile, destFile, (err) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve();
+    }),
+  );
 
-        sftp.fastPut(archivePath, '/home/wassburgare/build.tar.gz', (err) => {
+const runCommand = command =>
+  new Promise((resolve, reject) => {
+    connection.exec(command, (err, stream) => {
+      if (err) {
+        return reject(err);
+      }
+      return stream.pipe(devnull()).on('finish', () => resolve());
+    });
+  });
+
+tar.c({ gzip: true, file: archivePath }, ['build'])
+  .then(() =>
+    new Promise((resolve, reject) => {
+      connection.on('ready', () => {
+        connection.sftp((err, sftp) => {
           if (err) {
             return reject(err);
           }
 
-          connection.exec(commands.join(' && '), (err, stream) => {
-            if (err) {
-              return reject(err);
-            }
-
-            stream.pipe(devnull()).on('finish', () => {
+          console.log('Uploading build archive to server...');
+          return uploadFile(sftp, archivePath, '/home/wassburgare/build.tar.gz')
+            .then(() => {
+              console.log('Build archive was successfully uploaded.');
+              console.log('Updating website with new files...');
+              return Promise.resolve();
+            })
+            .then(() => runCommand(`tar -xvf ${serverHome}/build.tar.gz`))
+            .then(() => runCommand('rm -rf /var/www/guleboda.se/html/*'))
+            .then(() => runCommand(`mv ${serverHome}/build/guleboda.se/* /var/www/guleboda.se/html`))
+            .then(() => runCommand('rm -rf /var/www/admin.guleboda.se/html/*'))
+            .then(() => runCommand(`mv ${serverHome}/build/admin.guleboda.se/* /var/www/admin.guleboda.se/html`))
+            .then(() => runCommand(`rm -rf ${serverHome}/build`))
+            .then(() => runCommand(`rm ${serverHome}/build.tar.gz`))
+            .then(() => {
+              console.log('Website was successfully updated.');
               connection.end();
               return resolve();
             });
-          });
         });
+      }).connect({
+        host: 'guleboda.se',
+        username: 'wassburgare',
+        agent: process.env.SSH_AUTH_SOCK,
       });
-    }).connect({
-      host: 'guleboda.se',
-      username: 'wassburgare',
-      agent: process.env.SSH_AUTH_SOCK,
-    });
+    }),
+  )
+  .then(() => {
+    fs.unlinkSync(archivePath);
   });
-}).then(() => {
-  fs.unlink(archivePath);
-});
